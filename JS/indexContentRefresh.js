60fe0b7b6fc5240d361be201e61c089aac5e93a1
Cameron <cameronhunt88@hotmail.com>
2021-03-11 09:41:46 +0000

    Added necessary HTML files to HTML folder

--- JS/indexContentRefresh.js
+++ JS/indexContentRefresh.js
@@ -1,7 +1,18 @@
+/* Resources Used
+*/
 function hideMainMenu() {
-    document.getElementById("main-menu").style.visibility = "hidden";
+    document.getElementById("main-menu").style.display = "none";
     showNamePicker();
 }
 function showNamePicker() {
-    document.getElementById("name-picker").style.visibility = "visible";
+    document.getElementById("name-picker").style.display = "block";;
+}
+function previousPageButton() {
+    // Hide Name Picker Content
+    document.getElementById("name-picker").style.display = "none";
+    // Show Main Menu Content
+    document.getElementById("main-menu").style.display = "block";
+}
+function creditsPage() {
+    window.location.href = "../HTML/credits.html";
 }
