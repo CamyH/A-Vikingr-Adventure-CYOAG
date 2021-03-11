871d948e33a40c2ced5147fe7b428ebe99b1a6cd
Cameron <cameronhunt88@hotmail.com>
2021-03-11 10:12:55 +0000

    Content able to refresh on index + css & js

--- JS/indexContentRefresh.js
+++ JS/indexContentRefresh.js
@@ -7,12 +7,12 @@ function hideMainMenu() {
 function showNamePicker() {
     document.getElementById("name-picker").style.display = "block";;
 }
-function previousPageButton() {
+/*Only used for Index Page
+If user wants to go back to the main menu from the name picker content.
+*/
+function previousContentButton() {
     // Hide Name Picker Content
     document.getElementById("name-picker").style.display = "none";
     // Show Main Menu Content
     document.getElementById("main-menu").style.display = "block";
-}
-function creditsPage() {
-    window.location.href = "../HTML/credits.html";
 }
---
+++ JS/loadPages.js
@@ -0,0 +1,10 @@
+/* Resources Used
+https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelectorAll
+https://developer.mozilla.org/en-US/docs/Web/API/Window/location
+*/
+function backToIndex() {
+    window.location.href = "../index.html";
+}
+function creditsPage() {
+    window.location.href = "../HTML/credits.html";
+}
