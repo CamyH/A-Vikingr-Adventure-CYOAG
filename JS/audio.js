a97cc533dcffc2d048433c4bda284822df57b50f
Cameron <cameronhunt88@hotmail.com>
2021-03-19 15:29:00 +0000

    Addition to resources

--- JS/audio.js
+++ JS/audio.js
@@ -1,5 +1,6 @@
 /* Help Resources Used
 https://developer.mozilla.org/en-US/docs/Web/API/Web_Audio_API/Using_Web_Audio_API
+https://www.zapsplat.com/sound-effect-category/swords/page/6/
 */
 
 // legacy browser support
