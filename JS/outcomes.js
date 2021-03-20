53856c98f6dedb7735306bf1d4bde8c46d6d6c1a
Cameron <cameronhunt88@hotmail.com>
2021-03-20 10:58:46 +0000

    Adding outcome messages - not working yet

--- JS/outcomes.js
+++ JS/outcomes.js
@@ -1,30 +1,88 @@
 /*OUTCOMES FROM PAGE0*/
+var divideFoodChoice = "true";
 
-var divideFoodChoice = true;
-console.log(divideFoodChoice);
-divideFoodChoice = false;
-console.log(divideFoodChoice);
+// Hides Page 0 Content
+function option1() {
+    document.getElementById("page0").style.display = "none";
+    consequence1();
+}
+
+function option2() {
+    document.getElementById("page0").style.display = "none";
+    consequence2();
+}
+
+function consequence1() {
+    document.getElementById("outcomes").style.display = "block";
+    // Currently not working - message not displayed
+    var message;
+    message = "Dividing up the remaining food has given a morale boost to Sven's crew and they will fight better.";
+    document.getElementById("consequence1").innerHTML = message;
+}
+
+function consequence2() {
+    document.getElementById("outcomes").style.display = "block";
+    // Currently not working - message not displayed
+    var message;
+    message = "Saving the food has caused the crew’s morale to decrease.";
+    document.getElementById("consequence2").innerHTML = message;
+}
+
+// Testing Below
+/*
+var wload = window.onload;
 
 function test1() {
     window.location.href = "../HTML/page1.html";
+    var greeting;
+    greeting = "Dividing up the remaining food has given a morale boost to Sven's crew and they will fight better.";
+    document.getElementById("demo").innerHTML = greeting;
+    chooseGreeting("1");
 }
-
 function test2() {
     window.location.href = "../HTML/page1.html";
-    divideFoodChoice = false;
-    console.log("SAVE FOR LATER", divideFoodChoice);
+    setTimeout(test3, 1000);
+
+}
+function test3() {
+    chooseGreeting("2");
+    console.log("HEY");
+}
+function chooseGreeting(response) {
+    divideFoodChoice = response;
+    console.log(divideFoodChoice);
 }
 
-window.onload = function test(divideFoodChoice) {
-    console.log(divideFoodChoice);
-    if (divideFoodChoice) {
-        var textNode = document.createTextNode("Dividing up the remaining food has given a morale boost to Sven's crew and they will fight better.");
-        document.body.appendChild(textNode);
-    } else {
-        var textNode2 = document.createTextNode("Saving the food has caused the crew’s morale to decrease.");
-        document.body.appendChild(textNode2);
-    }
+function boolTest() {
+    var greeting;
+    greeting = "Saving the food has caused the crew’s morale to decrease.";
+    document.getElementById("demo").innerHTML = greeting;
 }
+
+console.log(divideFoodChoice);
+*/
+
+//boolTest();
+// window.onload = function test(divideFoodChoice) {
+//     console.log(divideFoodChoice);
+//     if (divideFoodChoice) {
+//         var textNode = document.createTextNode("Dividing up the remaining food has given a morale boost to Sven's crew and they will fight better.");
+//         document.body.appendChild(textNode);
+//     } else {
+//         var textNode2 = document.createTextNode("Saving the food has caused the crew’s morale to decrease.");
+//         document.body.appendChild(textNode2);
+//     }
+// }
+
+// window.onload = function myFunction() {
+//     var greeting;
+//     if (divideFoodChoice) {
+//         greeting = "Dividing up the remaining food has given a morale boost to Sven's crew and they will fight better.";
+//     } else {
+//         greeting = "Saving the food has caused the crew’s morale to decrease.";
+//     }
+//     document.getElementById("demo").innerHTML = greeting;
+// }
 
 // function divideFood() {
 //     // Progress to next page
