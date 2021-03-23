05f73c19211d5820d394d3b6778f9ce3f4cd0ed9
Cameron <cameronhunt88@hotmail.com>
2021-03-23 11:37:00 +0000

    More audio work - still not in working state

--- JS/audio.js
+++ JS/audio.js
@@ -7,3 +7,33 @@ https://www.zapsplat.com/sound-effect-category/swords/page/6/
 const AudioContext = window.AudioContext || window.webKitAudioContext;
 
 const audioContext = new AudioContext;
+
+// Get audio element
+const audioElement = document.querySelector('audio');
+
+// Pass into audio context
+const track = audioContext.createMediaElementSource(audioElement);
+
+track.connect(audioContext.destination);
+
+// Select play/pause button
+const playButton = document.querySelector('button');
+
+playButton.addEventListener('click'), function () {
+    // Check if context is in suspended state (autoplay policy)
+    if (audioContext.state == 'suspended')
+        audioContext.resume();
+
+    // Play/Pause track depending on state
+    if (this.dataset.playing == false) {
+        audioElement.play();
+        this.dataset.playing = 'true';
+    } else if (this.dataset.playing = 'true') {
+        audioElement.pause();
+        this.dataset.playing = 'false';
+    }
+}
+
+audioElement.addEventListener('ended', () => {
+    playButton.dataset.playing = 'false';
+}, false);
