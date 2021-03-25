7756d3e64d7b82aa1215826794f652f142ec7ad9
Cameron <cameronhunt88@hotmail.com>
2021-03-25 09:27:46 +0000

    Comprehensive Implementation of stats game + cleanup of other pages

--- JS/loadPages.js
+++ JS/loadPages.js
@@ -1,15 +1,21 @@
 /* Help Resources Used
 https://developer.mozilla.org/en-US/docs/Web/API/Window/location
 */
-// Loads Credits Page
-function creditsPage() { window.location.href = "../HTML/credits.html"; }
 // Loads Main Menu
 function mainMenu() { window.location.href = "../index.html"; }
+// Loads Credits Page
+function creditsPage() { window.location.href = "../HTML/credits.html"; }
 // Loads Page 3A
 function loadPage3a() { window.location.href = "/HTML/page3a.html"; }
 // Loads Page 3B
 function loadPage3b() { window.location.href = "/HTML/page3b.html"; }
 // Loads Page 3C
 function loadPage3c() { window.location.href = "/HTML/page3c.html"; }
+// Loads Page 4
+function loadPage4() { window.location.href = "/HTML/page4.html"; }
+// Loads Ending 1 
+function loadEnding1() { window.location.href = "/HTML/ending1.html"; }
+// Loads Ending 2
+function loadEnding2() { window.location.href = "/HTML/ending2.html"; }
 // Loads Playthough Stats Page
 function loadPlaythroughStats() { window.location.href = "/HTML/playthroughStats.html"; }
--- JS/playthroughStats.js
+++ JS/playthroughStats.js
@@ -3,8 +3,19 @@
 */
 
 // Playthrough Stats Page
-function stats() {
+// Load stats on page load
+window.onload = function stats() {
+    page0Stats();
+}
+
+function page0Stats() {
+    // Page 0
     var message = sessionStorage.getItem("consequence1");
     var div = document.getElementById("stats");
-    div.innerHTML += message;
+    if (message != null)
+        div.innerHTML += message;
+    else {
+        message = sessionStorage.getItem("consequence2");
+        div.innerHTML += message;
+    }
 }
