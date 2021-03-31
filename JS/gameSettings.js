c6ab1d925198f156b7535297debaf7d64d9d4d5c
Cameron <cameronhunt88@hotmail.com>
2021-03-31 09:45:49 +0100

    More work on save games, specifically loading them

--- JS/gameSettings.js
+++ JS/gameSettings.js
@@ -1,5 +1,7 @@
 /*Help Resources Used
 https://stackoverflow.com/questions/8832375/how-to-get-selected-value-from-dropdown-list-in-javascript/8832500
+https://stackoverflow.com/questions/49820106/add-a-button-using-javascript-to-an-existing-div
+https://stackoverflow.com/questions/5410745/how-can-i-get-a-list-of-the-items-stored-in-html-5-local-storage-from-javascript/5410827
 */
 // Set initial soldiers
 var soliders = 118;
@@ -18,6 +20,29 @@ function getName() {
     //alert(sessionStorage.getItem("playerName"));
 }
 
+// Save Games
 function loadGame() {
     document.getElementById("overlay").style.display = "block";
+    document.getElementById("close-button").style.display = "block";
+}
+
+function closeLoadGameMenu() {
+    document.getElementById("overlay").style.display = "none";
+    document.getElementById("close-button").style.display = "none";
+}
+
+function saveCheck() {
+    var overlay = document.getElementById("overlay");
+    localStorage.setItem("First Save", "1");
+    localStorage.setItem("Second Save", "2");
+    // Guard clause // Function will only continue if a save game exists.
+    //if (localStorage.getItem([0]) == null) return alert("No save games found.");
+
+    for (var i = 0; i < localStorage.length - 1; i++) {
+        var button = document.createElement("button");
+        button.innerText = localStorage.getItem;
+        overlay.appendChild(button);
+        //https://stackoverflow.com/questions/49820106/add-a-button-using-javascript-to-an-existing-div
+        //https://stackoverflow.com/questions/5410745/how-can-i-get-a-list-of-the-items-stored-in-html-5-local-storage-from-javascript/5410827
+    }
 }
