3548b4d978eddde088e1459d3733ee884c286643
Cameron <cameronhunt88@hotmail.com>
2021-04-13 10:34:26 +0100

    added: name personalisation for page1 story

--- JS/loadStory.js
+++ JS/loadStory.js
@@ -6,8 +6,14 @@ var playerName = sessionStorage.getItem("playerName");
 
 /* PAGE 1 */
 if (window.location.href.match("page1.html")) {
-    window.onload = function page1Message() {
+    window.onload = function page1Story() {
+        var mainStoryParagraph = document.getElementById("page1-story");
         var paragraph = document.getElementById("message");
+        mainStoryParagraph.textContent += ("Lucky the longship crashes into the east coast of England as "
+            + playerName + " and his Viking warriors rush onto the beach before them. They soon stop and look around, taking in their grey surroundings. In the distance, "
+            + playerName + "'s right-hand man and good friend, Egil,  spots a small plume of smoke and, in a loud, husky, voice shouts 'CAMP THERE!' while pointing his finger in the direction of the smoke. "
+            + playerName + " orders Egil and his men to follow him as they rush towards the billowing plume of smoke. As they get closer, the sound of men talking and laughing can be heard. "
+            + playerName + " orders his men to stop and listen. Egil creeps forward and soon spots a small number of soldiers sitting around a campfire and drinking ale. He retreats to their position and relays the intelligence to " + playerName)
         paragraph.textContent += playerName + " decides that we will attack these soldiers and find out who they are and where in England they have landed. How should you attack this camp?";
     }
 }
